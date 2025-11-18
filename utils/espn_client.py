523d97f1e319263182ee80ac1e3f88edf2bbb849
Tom Fijalkowski <tomfijalkowski@yahoo.com>
2025-11-17 21:01:23 -0500

    Initialize my league and team for testing

---
+++ utils/espn_client.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import os
+from espn_api.football import League
+
+from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
+from google.adk.tools.tool_context import ToolContext
+from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
+from mcp import StdioServerParameters
+
+load_dotenv()
+
+API_KEY = os.environ['GOOGLE_API_KEY']
+LEAGUE_ID = os.environ['ESPN_LEAGUE_ID']
+YEAR = 2025
+ESPN_S2 = os.environ['ESPN_S2_COOKIE']
+SWID = os.environ['ESPN_SWID_COOKIE']
+MY_TEAM_ID = os.environ['MY_TEAM_ID']
+
+league = League(league_id=LEAGUE_ID, year=YEAR, espn_s2=ESPN_S2, swid=SWID)
+
+my_team = league.teams[int(MY_TEAM_ID)-1]
+
+for player in my_team.roster:
+    if (player.name == 'Juwan Johnson'):
+        last_game_points = player.stats[11].get('points', 0.0) if len(player.stats) > 11 else 0.0
+        print(f"Name: {player.name}, Position: {player.position}, Injury status: {player.injuryStatus}, Total points scored: {player.stats[0]['points']}, Points scored last game: {last_game_points}")
+
+#{'Player was on bye' if player.active_status == 'bye' else player.stats[11]['points']}    
